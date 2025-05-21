dd08f9a844dfa42c36917da0dcd9acf05eb08eca
diixo <anatoliy.podoba@dxc.com>
2025-05-21 18:48:51 +0300

    Implementation log_db_user_access(..)

--- db_utils/db_test.py
+++ db_utils/db_test.py
@@ -1,6 +1,6 @@
 
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import (
@@ -15,7 +15,8 @@ from sqlalchemy import (
 )
 
 
-DB_NAME = "db-storage/access.db"
+DB_NAME = "db-storage/access-test.db"
+
 Base = declarative_base()
 
 
@@ -50,19 +51,51 @@ class AccessToken(Base):
     )
 
 
-def init_db():
-
-    #SessionLocal = sessionmaker(bind=engine)
-    #session = SessionLocal()
-
-    if not os.path.exists(DB_NAME):
-        Base = declarative_base()
-        engine = create_engine(f"sqlite:///{DB_NAME}")
+def init_db(db_name):
+    if not os.path.exists(db_name):
+        DATABASE_URL = f"sqlite:///{db_name}"
+        engine = create_engine(DATABASE_URL)
         Base.metadata.create_all(bind=engine)
         print("DB has been created")
     else:
         print("DB exists")
 
 
+def log_db_user_access(user_id, user_email, user_name, first_logged_in, last_accessed, token, db_name=DB_NAME):
+
+    init_db(db_name)
+
+    DATABASE_URL = f"sqlite:///{db_name}"
+
+    engine = create_engine(DATABASE_URL, echo=False)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = SessionLocal()
+    user = session.query(User).filter_by(email=user_email).first()
+
+    if not user:
+        user = User(
+            user_id=user_id,
+            email=user_email,
+            user_name=user_name,
+            first_logged_in=first_logged_in,
+            last_accessed=last_accessed
+        )
+        session.add(user)
+        session.commit()
+        print(f"Token has been added: {user_id}, {user_email}, {user_name}")
+
+    email_token = AccessToken(email=user_email, token=token)
+    session.add(email_token)
+    session.commit()
+    print(f"Token has been added: email={user_email}, token={token}")
+
+
+
 if __name__ == "__main__":
-    init_db()
+
+    first_logged_in=datetime.utcnow() - timedelta(hours=1)
+
+    log_db_user_access("2080", "airis@example.com", "Airis", first_logged_in, datetime.utcnow(), "token-2080-1170")
+    log_db_user_access("1600", "jenny@example.com", "Jenny", first_logged_in, datetime.utcnow(), "token-1600-0900")
+    log_db_user_access("1920", "johny@example.com", "Johny", first_logged_in, datetime.utcnow(), "token-1920-1080")
+    log_db_user_access("1280", "teddy@example.com", "Teddy", first_logged_in, datetime.utcnow(), "token-1280-0720")
